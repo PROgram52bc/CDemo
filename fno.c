cc792ffb8b144622624a9d68f15162eb72184b3e
David <daviddenghaotian@163.com>
2018-08-23 16:27:48 +0800

    fixed fno to create and delete(unlink) files automatically

--- fno.c
+++ fno.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-// NOTE: The absence of a file named zap.c will cause a segmentation fault.
-// So please create a file named 'zap.c' to see results
 int main() {
 	printf("file number of %s: %d\n", "stdout", fileno(stdout));
 	printf("file number of %s: %d\n", "stdin", fileno(stdin));
 	printf("file number of %s: %d\n", "stderr", fileno(stderr));
-	FILE *zap = fopen("zap.c","r");
-	printf("file number of %s: %d\n", "zap", fileno(zap));
+	int fd1 = creat("fnotmp1",0644);
+	int fd2 = creat("fnotmp2",0644);
+	FILE *f1 = fopen("fnotmp1","r");
+	FILE *f2 = fopen("fnotmp2","r");
+	printf("file number of %s: %d\n", "file1:", fileno(f1));
+	printf("file number of %s: %d\n", "file2:", fileno(f2));
+	unlink("fnotmp1");
+	unlink("fnotmp2");
 	return 0;
 }
